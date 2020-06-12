5883a2184ae2a12e28d4802e59bdb0371f0ca2d2
daitin <daitin239@gmail.com>
2020-06-12 17:30:00 +0100

    addgiaodien

--- src/contracts/DaiTokenMock.sol
+++ src/contracts/DaiTokenMock.sol
@@ -2,6 +2,10 @@ pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 
+  function setCompleted(uint completed) public restricted {
+    last_completed_migration = completed;
+  }
+
 contract DaiTokenMock is ERC20Mintable {
   string  public name;
   string  public symbol;
