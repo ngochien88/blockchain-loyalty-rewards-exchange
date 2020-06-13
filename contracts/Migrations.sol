9d473c6bca6b3bd188823992dea1a0bbcac9ddee
root <root@ksin.xksin>
2020-06-13 23:00:00 +0100

    init frontend + server

--- contracts/Migrations.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity >=0.4.21 <0.7.0;
-
-contract Migrations {
-  address public owner;
-  uint public last_completed_migration;
-}
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,13 @@
 const Migrations = artifacts.require("Migrations");
+const DaiTokenMock = artifacts.require("DaiTokenMock");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+module.exports = async function (deployer) {
+  await deployer.deploy(Migrations);
+  await deployer.deploy(DaiTokenMock);
+  const tokenMock = await DaiTokenMock.deployed();
+  // Mint 1,000 Dai Tokens for the deployer
+  await tokenMock.mint(
+    "0x8D3D8BA344F8B82BD5D2dD04517482e125B746aB",
+    "10000000000000000000000000"
+  );
 };
