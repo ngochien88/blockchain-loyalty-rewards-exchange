6527b2b7d597eeaead9b54073d36539b4c9f279b
tanphat <vanngunguvan@gmail.com>
2020-06-15 16:30:00 +0100

    bosung

--- src/contracts/DaiTokenMock.sol
+++ src/contracts/DaiTokenMock.sol
@@ -13,28 +13,3 @@ contract DaiTokenMock is ERC20Mintable {
     decimals = 18;
   }
 }
-
-constructor() public {
-    owner = msg.sender;
-  }
-
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
-
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
-  }
-
-  function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
-  }
-
-contract Migrations {
-  address public owner;
-  uint public last_completed_migration;
-
-  constructor() public {
-    owner = msg.sender;
-  }
