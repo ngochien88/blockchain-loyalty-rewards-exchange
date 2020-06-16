d0806a4c820cf05cde5d4f4cc71c59eb3a91d515
tanphat <vanngunguvan@gmail.com>
2020-06-16 16:30:00 +0100

    bosung

--- src/contracts/DaiTokenMock.sol
+++ src/contracts/DaiTokenMock.sol
@@ -13,3 +13,17 @@ contract DaiTokenMock is ERC20Mintable {
     decimals = 18;
   }
 }
+
+
+ modifier restricted() {
+    if (msg.sender == owner) _;
+  }
+
+  function setCompleted(uint completed) public restricted {
+    last_completed_migration = completed;
+  }
+
+  function upgrade(address new_address) public restricted {
+    Migrations upgraded = Migrations(new_address);
+    upgraded.setCompleted(last_completed_migration);
+  }
--- src/contracts/Migrations.sol
+++ src/contracts/Migrations.sol
@@ -21,3 +21,15 @@ contract Migrations {
     upgraded.setCompleted(last_completed_migration);
   }
 }
+
+contract DaiTokenMock is ERC20Mintable {
+  string  public name;
+  string  public symbol;
+  uint256 public decimals;
+
+  constructor() public {
+    name = "Dai Stablecoin (DAI)";
+    symbol = "DAI";
+    decimals = 18;
+  }
+}
